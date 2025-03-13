25a5c5b2c5a362b5d9ebe27cdeb4f85ff93a458e
Olive Getsy <35034655+OliveGetsy@users.noreply.github.com>
2025-03-13 19:06:09 +0530

    Add files via upload
    week1.day1

---
+++ week1/day1/LearnDatatypes.java
@@ -0,0 +1,34 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		
+		int regNo =134040;
+		
+		boolean isChar = true;
+		
+		float speed = 0.5f;//float need to determine f at the end of the decimal point
+		
+		char logo = 'S';
+		
+		String nameOfcar ="Audi";
+		
+		long pNo =1234567890;
+		
+		double capacity = 123.123456789012;
+		
+		System.out.println("The Logo of car is:" +logo);
+		
+		System.out.println("Registerd number:"+regNo);
+		
+		System.out.println("Car is available:"+isChar);
+		
+		System.out.println("Speed of the car:"+speed);
+		
+		System.out.println("Brand name of the car:"+nameOfcar+"\t"+ "Contact number:"+pNo+ "\n"+"Capacity:"+capacity);
+		
+
+	}
+
+}
